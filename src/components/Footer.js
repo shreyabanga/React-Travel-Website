2a3433a7790e8fab26826c5403733695b4a1c683
Shreya Banga <shreya.banga@gmail.com>
2021-11-22 19:00:06 -0500

    Initial Commit

---
+++ src/components/Footer.js
@@ -0,0 +1,122 @@
+import React from "react";
+import { Button } from "./Button";
+import { Link } from "react-router-dom";
+import "./Footer.css";
+function Footer() {
+  return (
+    <div className="footer-container">
+      <section className="footer-subscription">
+        <p className="footer-subscription-heading">
+          Join the Adventure newsletter to receiver our best vacation deals
+        </p>
+        <p className="footer-subscription-text">
+          You can unsubscribe at any time.{" "}
+        </p>
+        <div className="input-areas">
+          <form>
+            <input
+              type="email"
+              name="email"
+              placeholder="Your Email"
+              className="footer-input"
+            />
+            <Button buttonStyle="btn--outline">Subscribe</Button>
+          </form>
+        </div>
+      </section>
+
+      <div className="footer-links">
+        <div className="footer-link-wrapper">
+          <div className="footer-link-items">
+            <h2>About Us</h2>
+            <Link to="/sign-up">How it works</Link>
+            <Link to="/">Testimonials</Link>
+            <Link to="/">Careers</Link>
+            <Link to="/">Investors</Link>
+            <Link to="/">Terms of Service</Link>
+          </div>
+          <div className="footer-link-items">
+            <h2>Contact Us</h2>
+            <Link to="/sign-up">How it works</Link>
+            <Link to="/">Testimonials</Link>
+            <Link to="/">Careers</Link>
+            <Link to="/">Investors</Link>
+            <Link to="/">Terms of Service</Link>
+          </div>
+        </div>
+        <div className="footer-link-wrapper">
+          <div className="footer-link-items">
+            <h2>About Us</h2>
+            <Link to="/sign-up">How it works</Link>
+            <Link to="/">Testimonials</Link>
+            <Link to="/">Careers</Link>
+            <Link to="/">Investors</Link>
+            <Link to="/">Terms of Service</Link>
+          </div>
+          <div className="footer-link-items">
+            <h2>Contact Us</h2>
+            <Link to="/sign-up">How it works</Link>
+            <Link to="/">Testimonials</Link>
+            <Link to="/">Careers</Link>
+            <Link to="/">Investors</Link>
+            <Link to="/">Terms of Service</Link>
+          </div>
+        </div>
+      </div>
+      <section className="social-media">
+        <div className="social-media-wrap">
+          <div className="footer-logo">
+            <Link to="/" className="social-logo">
+              TRVL <i className="fab fa-typo3"></i>
+            </Link>
+          </div>
+          <small className="social-website-rights">TRVL 2020</small>
+          <div className="social-icons">
+            <Link
+              to="/"
+              targer="_blank"
+              aria-label="Facebook"
+              className="social-icon-link facebook"
+            >
+              <i className="fab fa-facebook"></i>
+            </Link>
+            <Link
+              to="/"
+              targer="_blank"
+              aria-label="Instagram"
+              className="social-icon-link instagram"
+            >
+              <i className="fab fa-instagram"></i>
+            </Link>
+            <Link
+              className="social-icon-link youtube"
+              to="/"
+              target="_blank"
+              aria-label="Youtube"
+            >
+              <i className="fab fa-youtube" />
+            </Link>
+            <Link
+              className="social-icon-link twitter"
+              to="/"
+              target="_blank"
+              aria-label="Twitter"
+            >
+              <i className="fab fa-twitter" />
+            </Link>
+            <Link
+              className="social-icon-link linkedin"
+              to="/"
+              target="_blank"
+              aria-label="LinkedIn"
+            >
+              <i className="fab fa-linkedin" />
+            </Link>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
+}
+
+export default Footer;
